e8d66e39493e26fc7a870d46746c7bddbf99b502
Yozhef <hisemjo@gmail.com>
2021-12-06 18:00:16 +0200

    feat: add phpunit test by collection

---
+++ tests/Collection/ExtendedMockHttpClientCollectionTest.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BehatHttpMockContext\Tests\Collection;
+
+use BehatHttpMockContext\Collection\ExtendedMockHttpClientCollection;
+use PHPUnit\Framework\TestCase;
+use TypeError;
+
+class ExtendedMockHttpClientCollectionTest extends TestCase
+{
+    public function testSuccess(): void
+    {
+        $clientCollection = new ExtendedMockHttpClientCollection([]);
+        
+        $this->assertCount(0, $clientCollection->getHandlers());
+    }
+    
+    public function testInitFailed(): void
+    {
+        $this->expectError(TypeError::class);
+        new ExtendedMockHttpClientCollection('string');
+    }
+    
+    public function testSetHandlersSuccess(): void
+    {
+        $clientCollection = new ExtendedMockHttpClientCollection([]);
+        
+        $this->assertCount(0, $clientCollection->getHandlers());
+        
+        $clientCollection->setHandlers([[]]);
+    
+        $this->assertCount(1, $clientCollection->getHandlers());
+    }
+    
+    public function testSetHandlersFailed(): void
+    {
+        $this->expectError(TypeError::class);
+        $clientCollection = new ExtendedMockHttpClientCollection([]);
+        
+        $this->assertCount(0, $clientCollection->getHandlers());
+        $clientCollection->setHandlers('string');
+    }
+    
+    public function testResetSuccess(): void
+    {
+        $clientCollection = new ExtendedMockHttpClientCollection([[]]);
+        
+        $this->assertCount(1, $clientCollection->getHandlers());
+        
+        $clientCollection->reset();
+        
+        $this->assertCount(0, $clientCollection->getHandlers());
+    }
+}
